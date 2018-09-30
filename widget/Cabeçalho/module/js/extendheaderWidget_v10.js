7d68c203974f0d879530f4a8f1ee0752a4e48a82
OrlandoAON <orlantunes@gmail.com>
2018-09-29 23:08:43 -0300

    initial structure

---
+++ widget/Cabeçalho/module/js/extendheaderWidget_v10.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview extendheaderWidget_v10.js.
+ *
+ * @author 
+ */
+define(
+  //---------------------
+  // DEPENDENCIES
+  //---------------------
+  [],
+
+  //-----------------------
+  // MODULE DEFINITION
+  //-----------------------
+  function () {
+ 
+    "use strict";
+    return {
+ 
+      onLoad: function(widget) {
+        //console.log('extendheaderWidget_v10.js onLoad');
+      }, 
+      
+      beforeAppear: function() {
+        //console.log('extendheaderWidget_v10.js before appear');
+      }
+    };
+  }
+);
---
+++ widget/Rodapé/module/js/extendfooter.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview extendfooter.js.
+ *
+ * @author 
+ */
+define(
+  //---------------------
+  // DEPENDENCIES
+  //---------------------
+  [],
+
+  //-----------------------
+  // MODULE DEFINITION
+  //-----------------------
+  function () {
+ 
+    "use strict";
+    return {
+ 
+      onLoad: function(widget) {
+        //console.log('extendfooter.js onLoad');
+      }, 
+      
+      beforeAppear: function() {
+        //console.log('extendfooter.js before appear');
+      }
+    };
+  }
+);
